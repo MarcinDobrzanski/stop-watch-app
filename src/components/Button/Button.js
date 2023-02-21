b9466c14c7acfb231dc65daf448eccbe732e2b1e
Marcin <marcin@dobrzanski.me>
2023-02-22 00:11:54 +0100

    Add style

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,20 +4,20 @@ import Timer from './../Timer/Timer';
 
 const Button = () => {
 
-  const [timeElapsed, setTimeElapsed] = useState(0);
+  const [time, setTime] = useState(0);
   const [timerActive, setTimerActive] = useState(false);
 
   useEffect(() => {
     let interval = null;
     if (timerActive) {
       interval = setInterval(() => {
-        setTimeElapsed(timeElapsed => timeElapsed + 1);
+        setTime(time => time + 1);
       }, 1);
-    } else if (!timerActive && timeElapsed !== 0) {
+    } else if (!timerActive && time !== 0) {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
-  }, [timerActive, timeElapsed]);
+  }, [timerActive, time]);
 
   const handleStart = () => {
     setTimerActive(true);
@@ -29,12 +29,12 @@ const Button = () => {
 
   const handleReset = () => {
     setTimerActive(false);
-    setTimeElapsed(0);
+    setTime(0);
   };
 
   return (
     <div>
-      <Timer timeElapsed={timeElapsed} />
+      <Timer time={time} />
       <button className={styles.button} onClick={handleStart}>Start</button>
       <button className={styles.button} onClick={handleStop}>Stop</button>
       <button className={styles.button} onClick={handleReset}>Reset</button>
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,10 +1,10 @@
 import styles from './Timer.module.scss'
 
-const Timer = ({ timeElapsed }) => {
-  const hours = Math.floor((timeElapsed / (1000 * 60 * 60)) % 24).toString().padStart(2, '0');
-  const minutes = Math.floor((timeElapsed / (1000 * 60)) % 60).toString().padStart(2, '0');
-  const seconds = Math.floor((timeElapsed / 1000) % 60).toString().padStart(2, '0');
-  const milliseconds = (timeElapsed % 1000).toFixed(0).toString().padStart(1, '0');
+const Timer = ({ time }) => {
+  const hours = Math.floor((time / (1000 * 60 * 60)) % 24).toString().padStart(2, '0');
+  const minutes = Math.floor((time / (1000 * 60)) % 60).toString().padStart(2, '0');
+  const seconds = Math.floor((time / 1000) % 60).toString().padStart(2, '0');
+  const milliseconds = (time % 1000).toFixed(0).toString().padStart(1, '0');
   const timeString = `${hours}:${minutes}:${seconds}:${milliseconds}`;
 
   return (
